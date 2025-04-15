9bcc08a8eee6fb6fae77a36960e6b94e6584f8cf
Nauman Ahmed <73252265+nauman117@users.noreply.github.com>
2025-04-14 17:40:57 -0700

    login required

--- src/componenets/Body.jsx
+++ src/componenets/Body.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from './Navbar'
-import { Outlet } from 'react-router'
+import { Outlet, useNavigate } from 'react-router'
 import Footer from './Footer'
+import { useDispatch, useSelector } from 'react-redux'
+import { BASE_URL } from '../utils/constants'
+import { addUser } from '../utils/userSlice'
+import axios from 'axios'
 
 const Body = () => {
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const userData = useSelector((store) => store.user);
+
+    const fetchUser = async () => {
+        try {
+            const res = await axios.get(BASE_URL + "/profile/view",
+                {
+                    withCredentials: true
+                }
+            );
+
+            dispatch(addUser(res.data));
+        } catch (err) {
+            if (err.status === 401) navigate("/login");
+            console.log(err);
+        }
+    }
+    useEffect(() => {
+        if (!userData) {
+            fetchUser()
+        }
+    }, [])
     return (
         <>
             <Navbar />
