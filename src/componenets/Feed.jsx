7859719ecde5b5ff5c66fad025409468d64ffa1f
Nauman Ahmed <73252265+nauman117@users.noreply.github.com>
2025-04-14 19:31:35 -0700

    built user card on feed

--- src/componenets/Feed.jsx
+++ src/componenets/Feed.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { BASE_URL } from '../utils/constants'
+import { useDispatch, useSelector } from 'react-redux'
+import { addFeed } from '../utils/feedSlice'
+import axios from 'axios'
+import UserCard from './UserCard'
 
 const Feed = () => {
+    const dispatch = useDispatch()
+    const feed = useSelector(store => store.feed)
+    console.log("🚀 ~ Feed ~ feed:", feed)
+
+    const getFeed = async () => {
+        if (feed) return
+        try {
+            const res = await axios.get(BASE_URL + "/feed", { withCredentials: true })
+            dispatch(addFeed(res?.data?.data))
+        } catch (err) {
+            console.log(err)
+        }
+    }
+    useEffect(() => {
+        getFeed()
+    }, [])
+
+    if (!feed) return null
     return (
-        <div>Feed</div>
+        <div className="flex justify-center m-10">
+            <UserCard user={feed[0]} />
+        </div>
     )
 }
 
